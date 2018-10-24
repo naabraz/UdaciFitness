7c4b1865714c7e326c775c8098c329aff94c0434
Natalia Braz <naabraz@gmail.com>
2018-10-24 14:29:10 -0300

    Adding redux

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
+import { connect } from 'react-redux'
 
-import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import { submitEntry, removeEntry } from '../utils/api'
+import { addEntry } from '../actions'
 
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
@@ -19,7 +21,7 @@ function SubmitBtn({ onPress }) {
   )
 }
 
-export default class AddEntry extends Component {
+class AddEntry extends Component {
   state = {
     run: 0,
     bike: 0,
@@ -62,7 +64,9 @@ export default class AddEntry extends Component {
     const key = timeToString()
     const entry = this.state
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: entry
+    }))
 
     this.setState(() => ({
       run: 0,
@@ -82,7 +86,9 @@ export default class AddEntry extends Component {
   reset = () => {
     const key = timeToString()
 
-    // Update Redux
+    this.props.dispatch(addEntry({
+      [key]: getDailyReminderValue()
+    }))
 
     // Route to Home
 
@@ -110,7 +116,6 @@ export default class AddEntry extends Component {
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()} />
-        <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -139,3 +144,13 @@ export default class AddEntry extends Component {
     )
   }
 }
+
+function mapStateToProps (state) {
+  const key = timeToString()
+
+  return {
+    alreadyLogged: state[key] && typeof state[key].today === 'undefined'
+  }
+}
+
+export default connect(mapStateToProps)(AddEntry)
