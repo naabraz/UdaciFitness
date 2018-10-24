3711ea98997a78a2a1bebaa0f0fbb3142ecb05ab
Natalia Braz <naabraz@gmail.com>
2018-10-24 13:13:44 -0300

    Adding async storage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,7 @@ import { View, TouchableOpacity, Text } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { submitEntry, removeEntry } from '../utils/api'
 
 import UdaciSlider from './UdaciSlider'
 import UdaciStepper from './UdaciStepper'
@@ -73,7 +74,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to 'DB'
+    submitEntry({ key, entry})
 
     // Clean local notification
   }
@@ -85,7 +86,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update 'DB'
+    removeEntry(key)
   }
 
   render() {
