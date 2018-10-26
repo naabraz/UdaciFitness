49d61e1f76406f276847784d16e1bd5b941869ea
Natalia Braz <naabraz@gmail.com>
2018-10-26 16:16:45 -0300

    Creating calendar

--- components/History.js
+++ components/History.js
@@ -6,6 +6,8 @@ import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
 
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+
 class History extends Component {
   componentDidMount() {
     const { dispatch } = this.props
@@ -21,11 +23,32 @@ class History extends Component {
       })
   }
 
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(metrics)}</Text>
+      }
+    </View>
+  )
+
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No Data for this day</Text>
       </View>
+    )
+  }
+
+  render() {
+    const { entries } = this.props
+
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 }
