e3d51115d7e938330ce6d00577e92b7aed258b09
Natalia Braz <naabraz@gmail.com>
2018-10-26 16:45:38 -0300

    Adding style to date inside history

--- components/History.js
+++ components/History.js
@@ -9,6 +9,7 @@ import { white } from '../utils/colors'
 
 import UdaciFitnessCalendar from 'udacifitness-calendar'
 import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
 
 class History extends Component {
   componentDidMount() {
@@ -35,7 +36,7 @@ class History extends Component {
             </Text>
           </View>
         : <TouchableOpacity onPress={() => console.log('Pressed!')}>
-            <Text>{JSON.stringify(metrics)}</Text>
+            <MetricCard metrics={metrics} date={formattedDate} />
           </TouchableOpacity>
       }
     </View>
