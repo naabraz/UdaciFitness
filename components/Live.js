e40e4048213fabf570a08a8511d0e9fa367dadcf
Natalia Braz <naabraz@gmail.com>
2018-10-31 15:57:51 -0300

    Adding animation

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { Location, Permissions } from 'expo';
 
@@ -11,6 +11,7 @@ export default class Live extends Component {
     coords: null,
     status: null,
     direction: '',
+    bounceValue: new Animated.Value(1)
   }
 
   componentDidMount() {
@@ -52,7 +53,14 @@ export default class Live extends Component {
       distanceInterval: 1,
     }, ({coords}) => {
       const newDirection = calculateDirection(coords.heading)
-      const { direction } = this.state
+      const { direction, bounceValue } = this.state
+
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+        ]).start()
+      }
 
       this.setState(() =>({
         coords,
@@ -63,7 +71,7 @@ export default class Live extends Component {
   }
 
   render() {
-    const { status, coords, direction } = this.state
+    const { status, coords, direction, bounceValue } = this.state
 
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}} />
@@ -102,9 +110,9 @@ export default class Live extends Component {
           <Text style={styles.header}>
             You're heading
           </Text>
-          <Text style={styles.direction}>
+          <Animated.Text style={[styles.direction, { transform: [{ scale: bounceValue }] }]}>
             {direction}
-          </Text>
+          </Animated.Text>
         </View>
 
         <View style={styles.metricContainer}>
