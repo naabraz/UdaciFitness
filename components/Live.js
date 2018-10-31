e304ce86ac2b2a0454c051117462a24d9c1c68db
Natalia Braz <naabraz@gmail.com>
2018-10-31 11:22:20 -0300

    Creating method to set location

--- components/Live.js
+++ components/Live.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
+import { Location, Permissions } from 'expo';
 
 import { purple, white } from '../utils/colors'
+import { calculateDirection } from '../utils/helpers';
 
 export default class Live extends Component {
   state = {
@@ -11,8 +13,41 @@ export default class Live extends Component {
     direction: '',
   }
 
+  componentDidMount() {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting Location permission: ', error)
+
+        this.setState(() => ({ status: 'undetermined' }))
+      })
+  }
+
   askPermission = () => {
 
+  }
+
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+      distanceInterval: 1,
+    }, ({coords}) => {
+      const newDirection = calculateDirection(coords.heading)
+      const { direction } = this.state
+
+      this.setState(() =>({
+        coords,
+        status: 'granted',
+        direction: newDirection,
+      }))
+    })
   }
 
   render() {
